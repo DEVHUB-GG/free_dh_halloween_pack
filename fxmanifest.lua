1ebbb5133e4aacc8535b2888f5966f1d4f917591
w. <71177337+Wiktor01@users.noreply.github.com>
2024-09-25 13:51:11 +0200

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'cerulean'
+games {'gta5'}
+lua54 'yes'
+
+author 'DEVHUB (store.devhub.gg)'
+description 'FREE HALLOWEEN PACK'
+version '1.0.0'
+
+files {
+    "stream/dh_halloween_free_pack.ytyp",
+}
+
+data_file "DLC_ITYP_REQUEST" "stream/dh_halloween_free_pack.ytyp"
